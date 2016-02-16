fb9477bcb99c8cf6ab303d16dcdbe8fd57da2e16
Masato Shimizu <ma6ato@gmail.com>
2016-02-16 13:40:17 +0900

    basic.jsonのインデントを廃止

--- srv/salt/config/salt/device/basic.py
+++ srv/salt/config/salt/device/basic.py
@@ -4,11 +4,11 @@ import json
 import random
 
 basic = '/root/iot_apps/meshblu-compose/manage/data/basic.json'
-user='idcf'
+user = 'idcf'
 num = 8
 arr = list('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 password = ''.join([random.choice(arr) for i in range(num)])
 data = dict(user=user, password=password)
 
 with open(basic, 'w') as f:
-    json.dump(data, f, indent=4)
+    json.dump(data, f)
